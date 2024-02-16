10c1519a147bcfa51f281f65d498b04ca1fbb766
Sha-hed <shahedcse14@gmail.com>
2024-02-16 15:04:55 +0600

    scripting part 1

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function play() {
+  const homeSection = document.getElementById("home");
+  homeSection.classList.add("hidden");
+
+  const playSection = document.getElementById("playground");
+  playSection.classList.remove("hidden");
+}
